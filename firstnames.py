2bfb1e7cdb478c5d07de279ff9dc9f40007e33ec
RSago <rsago@ualr.edu>
2022-09-07 21:02:19 +0000

    First file

--- firstnames.py
+++ firstnames.py
@@ -1,2 +1,4 @@
+raw_input("First Name:")
+
 fname = input("First Name : ")
 print ("Hello  "+ fname)
---
+++ lastdigit.py
@@ -0,0 +1,5 @@
+
+def last_digit(n):
+    n = str(n)
+int(n[len(n)-1])
+print(last_digit(int(input("Enter A Number: "))))

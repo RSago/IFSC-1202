6a872da8df150bbd355b515d4a6965aab20942ba
RSago <rsago@ualr.edu>
2022-09-12 15:30:57 +0000

    First file

---
+++ unitconversion.py
@@ -0,0 +1,12 @@
+if FromUnit == "ft" and ToUnit == "yd": 
+    multiplier = 0.3333333333
+if FromUnit == "ft" and ToUnit == "in":
+    multiplier - 12.0
+
+d_inches = d_ft * 12
+d_yards = d_ft / 3.0
+d_miles = d_ft / 5280.0    
+FromValue 
+print("Enter From Value:")
+print("Enter From Unit(in, ft, yd, mi):")
+print("Enter To Unit(in, ft, yd, mi): ")

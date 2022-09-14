a7c08db5ba12897b43d31c36db048d93e2497bd7
RSago <rsago@ualr.edu>
2022-09-14 15:34:42 +0000

    First File

---
+++ leapyear.py
@@ -0,0 +1,7 @@
+input("Enter Year:")
+if(year % 400 == 0) and (year % 100 == 0):
+    print("{0} is a leap year".format.(year))
+elif (year % 4 == 0) and (year % 100 != 0):
+    print("{0} is a leap year".format.(year))
+else:
+    print("{0} is a common year".format.(year))

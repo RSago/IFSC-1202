b6d335244510109b5184530ba29066e314a4b25c
RSago <rsago@ualr.edu>
2022-09-21 14:42:12 +0000

    First File

--- examone.py
+++ examone.py
@@ -3,17 +3,18 @@ human=0
 computer=0
 for i in range(5):
     print("Round:{}"(i+1))
-humanNumber=int(input("Enter Your Guess: "))
-computerGeneratedNumber=random.randint(1,5)
-if (humanNumber+computerGeneratedNumber)%2==0:
-    human=human+1
-else:
-    computer=computer+1
-if (humanNumber+computerGeneratedNumber)%2==0:
-    print("Sum is even.")
-else:
-    print("Sum is odd")
-print("Human Score: {}\nComputer Score: {}".format(human,computer))
+    humanNumber=int(input("Enter Your Guess: "))
+    computerGeneratedNumber=random.randint(1,5)
+    if (humanNumber+computerGeneratedNumber)%2==0:
+        human=human+1
+    else:
+        computer=computer+1
+    print('Human Guess: {}\nComputer Guess: {}'.format(humanNumber,computerGeneratedNumber))
+    if (humanNumber+computerGeneratedNumber)%2==0:
+        print("Sum is even.")
+    else:
+        print("Sum is odd")
+    print("Human Score: {}\nComputer Score: {}".format(human,computer))
 if human>computer:
     print("Human Wins")
 else:

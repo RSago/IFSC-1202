ae956fafdd45a19db428576ab3a9db52dbc83e4f
RSago <rsago@ualr.edu>
2022-10-05 18:10:28 +0000

    first file#

---
+++ secondoccurrence.py
@@ -0,0 +1,10 @@
+s = input("Enter a string: ")
+index = s.find('f')
+if index == -1:
+    print("Zero f")
+else:
+    index = s.find('f', index + 1)
+    if index == -1:
+        print("One f")
+    else:
+        print(index)

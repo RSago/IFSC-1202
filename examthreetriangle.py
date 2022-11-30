d8f6f6b112c8e748c40e03da857bf8c2524ae4aa
RSago <rsago@ualr.edu>
2022-11-30 16:47:04 +0000

    First file

--- examthreetriangle.py
+++ examthreetriangle.py
@@ -1,3 +1,6 @@
+from math import sqrt 
+from math import acos
+from math import pi
 class Triangle:
     num = 0
     def __init__(self, side, point):
@@ -8,17 +11,38 @@ class Triangle:
         return 4 * self.side
     def area(self):
         return self.side * self.side
-    def move(self, newPoint):
+    def type(self, newPoint):
         oldPoint = self.point
         x = oldPoint[0] + newPoint[0]
         y = oldPoint[1] + newPoint[1]
         self.point = (x, y)
-    def __str__(self):
-        return "Square of {0} at {1}".format(self.side, self.point)
+    def angle(self):
+        return "Angle".format(self.side, self.point)
 
-s1 = Triangle (8, (3, 5))
+#square length of sides 1, 2, and 3
+s1 = lengthSquare(B,C)
+s2 = lengthSquare(A,C)
+s3 = lengthSquare(A,B)
+	
+# length of sides be 1, 2, 3
+s1 = (float)sqrt (1)
+s2 = (float)sqrt (2)
+s3 = (float)sqrt (3)
+	
+# From Cosine law
+s1 = (float) acos ((b2 + c2 - a2)/(2*b*c))
+s2 = (float) acos ((a2 + c2 - b2)/(2*a*c))
+s3 = (float) acos ((a2 + b2 - c2)/(2*a*b))
+	
+# Converting to degree
+s1 = (float) (s1 * 180 / PI)
+s2 = (float) (s2 * 180 / PI)
+s3 = (float) (s3 * 180 / PI)
+
+s1 = Triangle (3, (4, 5))
 print(s1)
 print("Area : ", s1.area())
+
 print("Perimeter : ", s1.perimeter())
 print("Angle 1")
 print ("Angle 2")
@@ -26,4 +50,4 @@ print ("Angle 3")
 s1.move((-1, 1))
 print(s1)
 print("Type : ", Triangle.num)
-s2 = Angle (10, (-2, -6))
+s2 = Angle (3, (3, 5))
